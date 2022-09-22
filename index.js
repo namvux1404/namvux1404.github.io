ccc850066b84630d7dfafe9d32b9a7d68197c66c
Nam Vu <van.nam.vu@umontreal.ca>
2022-09-23 04:40:51 +0900

    update website

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Typer = {
   text: '',
   accessCountimer: null,
   index: 0,
-  speed: 2,
+  speed: 5,
   file: '',
   accessCount: 0,
   deniedCount: 0,
@@ -95,7 +95,7 @@ function replaceUrls(text) {
   }
 }
 
-Typer.speed = 3;
+Typer.speed = 5;
 Typer.file = 'namvux.txt';
 Typer.init();
 
